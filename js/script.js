ac2703c3a7b42be8e664b6047e7d43562a97e97c
kostya <matvienkoigor956@gmail.com>
2020-08-17 15:00:33 +0300

    added animation to form response

--- js/script.js
+++ js/script.js
@@ -4,3 +4,31 @@ $(".nav_link").on("click", function(event) {
         top = $(id).offset().top;
     $('body,html').animate({ scrollTop: top }, 500);
 });
+
+
+
+//------------------------------------------------- ajax
+
+function submit_form(id) {
+    var form = $('#' + id);
+    var res = $('#ajax_respounce');
+    form.parent().animate({
+            opacity: 0,
+            left: "20%"
+        },
+        800, () => {
+            setTimeout(() => {
+                form.hide();
+                res.show();
+                form.parent().animate({
+                        paddingTop: 100,
+                        paddingBottom: 100,
+                        opacity: 1,
+                        left: 0
+                    },
+                    800);
+            }, 400)
+
+        }
+    )
+}
