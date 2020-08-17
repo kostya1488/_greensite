962ede6ad6b29f18e31f3b33437a200404170e93
kostya <matvienkoigor956@gmail.com>
2020-08-17 12:14:51 +0300

    changed toggle button position

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(".nav_link").on("click", function(event) {
+    event.preventDefault();
+    var id = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({ scrollTop: top }, 500);
+});
